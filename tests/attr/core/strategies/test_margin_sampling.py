c7a5947c6f271676f2d1430e112882f5df3a14ad
Masanari Kimura <noconoco.lib@gmail.com>
2019-12-14 11:38:58 -0800

    add margin sampling test

---
+++ tests/attr/core/strategies/test_margin_sampling.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import numpy as np
+import tensorflow as tf
+
+from orakl.attr import MarginSampling
+
+from ...helpers.utils import BaseTest
+
+
+class Test(BaseTest):
+    def test_call_with_empty_data_pool(self):
+        ms = MarginSampling()
+        model = tf.keras.Model()
+
+        with self.assertRaises(AssertionError):
+            ms(model)
+
+
+    def test_call_with_random_data_pool(self):
+        n_samples = 10
+        n_classes = 3
+
+        ms = MarginSampling()
+
+        initializer = tf.initializers.he_normal(seed=0)
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(
+                n_classes,
+                input_shape=(10,),
+                kernel_initializer=initializer),
+        ])
+
+        loss_function = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True,
+            reduction=tf.keras.losses.Reduction.NONE)
+
+        data_pool = np.random.rand(100, 10)
+        indexes, samples = ms(model,
+                               loss_function=loss_function,
+                               n_classes=n_classes,
+                               data_pool=data_pool,
+                               n_samples=n_samples)
+
+        assert(len(indexes) == n_samples)
+        assert(len(samples) == n_samples)
