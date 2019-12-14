c17639d71528e5871b76e39eee2ac7f2563eb265
Masanari Kimura <noconoco.lib@gmail.com>
2019-12-14 11:25:55 -0800

    add margin sampling

---
+++ orakl/attr/core/strategies/margin_sampling.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import heapq
+import tqdm
+import numpy as np
+import tensorflow as tf
+
+from .strategy import BasePoolStrategy
+from ...utils.functions import grad
+
+
+class MarginSampling(BasePoolStrategy):
+    r"""Implements Margin Sampling algorithm.
+    """
+
+    def __call__(self,
+                 current_model,
+                 data_pool=None,
+                 excluded_indexes=None,
+                 loss_function=None,
+                 n_classes=None,
+                 n_samples=10,
+                 batch_size=32):
+
+        if data_pool is None and self.data_pool is None:
+            raise AssertionError("data pool is empty!")
+
+        data_pool_ = data_pool if data_pool is not None else self.data_pool
+
+        if excluded_indexes is not None:
+            excluded_indexes_ = excluded_indexes
+        elif self.excluded_indexes is not None:
+            excluded_indexes_ = self.excluded_indexes
+        else:
+            excluded_indexes_ = []
+
+        n_data_pool = len(list(data_pool))
+        candidates = [i for i in range(0, n_data_pool) if i not in excluded_indexes_]
+        x = tf.convert_to_tensor(data_pool_[candidates])
+        preds = tf.reduce_max(current_model(x), 1)
+        indexes = heapq.nsmallest(n_samples,
+                                  range(len(candidates)),
+                                  preds.__getitem__)
+
+        samples = data_pool_[indexes]
+
+        return indexes, samples
